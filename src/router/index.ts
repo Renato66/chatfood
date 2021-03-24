aa45725bddd651caabd4ff6cf2d607eeb6c436b3
renato.santos <renato.santos@doc88.com.br>
2021-03-23 22:46:56 -0300

    chore: update routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,18 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Menu',
+    component: () => import(/* webpackChunkName: "Menu" */ '../views/Menu.vue')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '*',
+    name: '404',
+    component: () => import(/* webpackChunkName: "404" */ '../views/404.vue')
   }
 ]
 
---
+++ src/views/404.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>Ops... 404.. Page not found!</h1>
+    <h2>Return to <router-link to="/">Menu</router-link></h2>
+  </div>
+</template>
