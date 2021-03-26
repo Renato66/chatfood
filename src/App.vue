0288940882e1fe23eeb90ba807172668abb39a54
renato.santos <renato.santos@doc88.com.br>
2021-03-26 00:17:45 -0300

    feat: add transitions

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view/>
+    <transition name="component-fade" mode="out-in">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
@@ -10,6 +12,14 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: $black;
+}
+
+.component-fade-enter-active, .component-fade-leave-active {
+  transition: opacity .3s ease;
+}
+.component-fade-enter, .component-fade-leave-to
+/* .component-fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 
 </style>
