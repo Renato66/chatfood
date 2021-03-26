551e40948475d1aeb7f426d47311b5c5bcf0646f
renato.santos <renato.santos@doc88.com.br>
2021-03-26 16:21:01 -0300

    fix: compare to lower case

--- src/helpers/utils/compareStrings.ts
+++ src/helpers/utils/compareStrings.ts
@@ -1,4 +1,4 @@
 export const compareStrings = (search: string) => {
-  const normalizedSearch = search.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
-  return function comparizon (text: string): boolean { return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '').includes(normalizedSearch) }
+  const normalizedSearch = search.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase()
+  return function comparizon (text: string): boolean { return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase().includes(normalizedSearch) }
 }
