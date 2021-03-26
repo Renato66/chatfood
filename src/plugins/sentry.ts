f49d622e85342a814358ae6d1a518cd0e8572f06
renato.santos <renato.santos@doc88.com.br>
2021-03-26 16:28:00 -0300

    chore: add sentry to development mode

--- src/plugins/sentry.ts
+++ src/plugins/sentry.ts
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import * as Sentry from '@sentry/vue'
 import { Integrations } from '@sentry/tracing'
-if (process.env.NODE_ENV !== 'development') {
-  Sentry.init({
-    Vue,
-    dsn: 'https://29354945951a4109903c8ecbb7e34806@o557645.ingest.sentry.io/5690142',
-    integrations: [new Integrations.BrowserTracing()],
 
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0
-  })
-}
+Sentry.init({
+  Vue,
+  dsn: 'https://29354945951a4109903c8ecbb7e34806@o557645.ingest.sentry.io/5690142',
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0
+})
